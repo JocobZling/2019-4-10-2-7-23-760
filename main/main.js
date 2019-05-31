899abc21f7717e5cdbe6651bfa189be0a4ba4055
JocobZling <924798429@qq.com>
2019-05-31 23:13:53 +0800

    Finish text fuction demo1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+  //{distance:1,parkTime:5}
+  let distance = inputs.distance;
+  let parkTime = inputs.parkTime;
+  let price;
+  if (distance <= 2) {
+    price = 6 + parkTime * 0.25;
+  } else if (distance > 2 && distance <= 8) {
+    price = 6 + (distance - 2) * 0.8 + parkTime * 0.25;
+  } else if (distance > 8) {
+    price = 6 + (distance - 8) * 0.8 * 1.5 + 6 * 0.8 + parkTime * 0.25;
+  }
+  return Math.round(price);
 };
